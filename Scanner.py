e065cf6471a604aa5500be6442da318f7675cb9e
lu-dwig <murimiludwig@gmail.com>
2025-01-28 18:45:09 +0300

    creation of if statement for tcp scan

--- Scanner.py
+++ Scanner.py
@@ -18,6 +18,14 @@ resp = input("""\nPlease enter the type of scan you want to run
                 3)Comprehensive Scan \n""")
 print("You have selected option: ", resp)
 
+if resp == '1' :
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr,"1-1024",'-v -sS') #the # are port range to scan, the last part is the scan type
+    print(scanner.scaninfo())
+    print("Ip Status: " , scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: " , scanner[ip_addr]['tcp'].keys())    
+
 resp_dict={'1':['-v -sS','tcp'],'2':['-v -sU','udp'],'3':['-v -sS -sV -sC -A -O','tcp']}
 if resp not in resp_dict.keys():
     print("enter a valid option")
@@ -26,6 +34,6 @@ else:
     scanner.scan(ip_addr,"1-1024",resp_dict[resp][0]) #the # are port range to scan, the last part is the scan type
     print(scanner.scaninfo())
     if scanner.scaninfo()=='up':
-        print("Scanner Status: ",scanner[ip_addr].state())
+        print("Ip Scanner Status: ",scanner[ip_addr].state())
         print(scanner[ip_addr].all_protocols())
         print("Open Ports: ",scanner[ip_addr][resp_dict[resp][1]].keys())  #display all open ports
