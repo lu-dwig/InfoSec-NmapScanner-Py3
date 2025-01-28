7d453de77125d037708eae1aa7238a3ab66228f9
lu-dwig <murimiludwig@gmail.com>
2025-01-28 18:26:11 +0300

    response for the user once prompted fot the san

--- Scanner.py
+++ Scanner.py
@@ -11,3 +11,9 @@ ip_addr = input("Please enter the IP address you want to scan: ")
 print("The IP you entered is: ", ip_addr)
 type(ip_addr)
 
+
+resp = input("""\nPlease enter the type of scan you want to run
+                1)SYN ACK Scan
+                2)UDP Scan
+                3)Comprehensive Scan \n""")
+print("You have selected option: ", resp)
