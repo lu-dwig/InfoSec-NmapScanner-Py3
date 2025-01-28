471e7ab1230874755705e54a34fb8011b4d73708
lu-dwig <murimiludwig@gmail.com>
2025-01-28 18:24:15 +0300

    prompting of the user for the ip they want to scann

--- Scanner.py
+++ Scanner.py
@@ -7,3 +7,7 @@ scanner = nmap.PortScanner()
 print("Welcome, this is a simple nmap automation tool")
 print("<----------------------------------------------------->")
 
+ip_addr = input("Please enter the IP address you want to scan: ")
+print("The IP you entered is: ", ip_addr)
+type(ip_addr)
+
