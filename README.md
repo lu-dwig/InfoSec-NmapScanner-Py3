162ad03d8caacdcc3e45bfdbfd04e555d5ce9a52
lu-dwig <murimiludwig@gmail.com>
2025-01-28 18:56:08 +0300

    commenting out of the unused code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # InfoSec-NmapScanner-Py3
+
+Simple network scanner - built with Python3 and Nmap.
+
--- Scanner.py
+++ Scanner.py
@@ -18,30 +18,30 @@ resp = input("""\nPlease enter the type of scan you want to run
                 3)Comprehensive Scan \n""")
 print("You have selected option: ", resp)
 
-if resp == '1' :
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr,"1-1024",'-v -sS') #the # are port range to scan, the last part is the scan type
-    print(scanner.scaninfo())
-    print("Ip Status: " , scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: " , scanner[ip_addr]['tcp'].keys())    
-elif resp == '2':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr,"1-1024",'-v -sU') #the # are port range to scan, the last part is the scan type
-    print(scanner.scaninfo())
-    print("Ip Status: " , scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: " , scanner[ip_addr]['udp'].keys())
-elif resp == '3':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr,"1-1024",'-v -sS -sV -sC -A -O') #the # are port range to scan, the last part is the scan type
-    print(scanner.scaninfo())
-    if scanner.scaninfo()=='up':
-        print("Ip Status: " , scanner[ip_addr].state())
-        print(scanner[ip_addr].all_protocols())
-        print("Open Ports: " , scanner[ip_addr]['tcp'].keys())  #display all open ports
-elif resp >= '4':
-    print("Invalid option, please enter a number between 1 and 3")
+# if resp == '1' :
+#     print("Nmap Version: ", scanner.nmap_version())
+#     scanner.scan(ip_addr,"1-1024",'-v -sS') #the # are port range to scan, the last part is the scan type
+#     print(scanner.scaninfo())
+#     print("Ip Status: " , scanner[ip_addr].state())
+#     print(scanner[ip_addr].all_protocols())
+#     print("Open Ports: " , scanner[ip_addr]['tcp'].keys())    
+# elif resp == '2':
+#     print("Nmap Version: ", scanner.nmap_version())
+#     scanner.scan(ip_addr,"1-1024",'-v -sU') #the # are port range to scan, the last part is the scan type
+#     print(scanner.scaninfo())
+#     print("Ip Status: " , scanner[ip_addr].state())
+#     print(scanner[ip_addr].all_protocols())
+#     print("Open Ports: " , scanner[ip_addr]['udp'].keys())
+# elif resp == '3':
+#     print("Nmap Version: ", scanner.nmap_version())
+#     scanner.scan(ip_addr,"1-1024",'-v -sS -sV -sC -A -O') #the # are port range to scan, the last part is the scan type
+#     print(scanner.scaninfo())
+#     if scanner.scaninfo()=='up':
+#         print("Ip Status: " , scanner[ip_addr].state())
+#         print(scanner[ip_addr].all_protocols())
+#         print("Open Ports: " , scanner[ip_addr]['tcp'].keys())  #display all open ports
+# elif resp >= '4':
+#     print("Invalid option, please enter a number between 1 and 3")
 
 resp_dict={'1':['-v -sS','tcp'],'2':['-v -sU','udp'],'3':['-v -sS -sV -sC -A -O','tcp']}
 if resp not in resp_dict.keys():
